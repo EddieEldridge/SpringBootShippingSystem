5815f52f9700e406c33c639d7c640dab4f9b5db0
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-04-16 22:28:49 +0100

    Added empty interface class files

---
+++ src/main/java/com/ships/repositories/OrderInterface.java
@@ -0,0 +1,9 @@
+package com.ships.repositories;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public class OrderInterface {
+
+}
